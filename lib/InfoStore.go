730bc32cb484f468ac9294acee89bbaf6840e2dd
Sameer Chandra Tatiraju <statiraju@vmware.com>
2019-12-11 15:32:23 +0530

    addd changes

--- lib/InfoStore.go
+++ lib/InfoStore.go
@@ -1,44 +1,39 @@
 package InfoStore
 
 import (
-	"log"
 	"fmt"
+	"log"
+
 	bolt "go.etcd.io/bbolt"
 )
 
 type InfoPair struct {
-	Key string
+	Key   string
 	Value string
 }
 
-
 func Write(server string) {
 
-
-db, err := bolt.Open("info.db", 0666, nil)
-if err != nil {
-  log.Fatal(err)
-}
-defer db.Close()
-
-db.Update(func(tx *bolt.Tx) error {
-
-	_, err := tx.CreateBucketIfNotExists([]byte("agentInfo"))
+	db, err := bolt.Open("info.db", 0666, nil)
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
+	defer db.Close()
 
+	db.Update(func(tx *bolt.Tx) error {
 
-	bucket := tx.Bucket([]byte("agentInfo"))
-	error := bucket.Put([]byte("server"), []byte(server))
-	
+		_, err := tx.CreateBucketIfNotExists([]byte("agentInfo"))
+		if err != nil {
+			return err
+		}
 
-
+		bucket := tx.Bucket([]byte("agentInfo"))
+		error := bucket.Put([]byte("server"), []byte(server))
 
 		v := bucket.Get([]byte("server"))
 		fmt.Printf("The answer is: %s\n", v)
 		return error
 
-})
+	})
 
 }
